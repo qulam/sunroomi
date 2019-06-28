1f43bf8c6b5809821eb9b2e9b2a29eb7d3d3134a
qulam <qulamelisoy@mail.ru>
2019-06-28 18:02:10 +0400

    first push to git hub

---
+++ index.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <!--Style-->
+    <link rel="stylesheet" href="media/css/bootstrap.min.css">
+    <link rel="stylesheet" href="media/css/font-awesome.min.css">
+    <link rel="stylesheet" href="media/css/fonts.css">
+    <link rel="stylesheet" href="media/css/all.min.css">
+    <link rel="stylesheet" href="media/css/style.css">
+    <!-- /Style-->
+    <title>Sunroomi</title>
+</head>
+<body>
+
+
+
+<!--Script files-->
+<script src="media/js/jquery.min.js"></script>
+<script src="media/js/bootstrap.min.js"></script>
+<script src="media/js/main.js"></script>
+<!-- /Script files-->
+</body>
+</html>
